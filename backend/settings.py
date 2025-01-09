293afb825f9e69ee4b01a59a94c434c9dffa7a51
Rehman Aziz <rehmanaziz1323@gmail.com>
2025-01-09 21:07:03 +0500

    commented static files config because not required in api only project

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     'authentication',
     'rest_framework',
     'corsheaders',
@@ -148,7 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
